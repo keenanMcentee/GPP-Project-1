c5ea8b33b18c61c1610c268e2b53840de35aa48d
MuddyGames <make@muddygames.com>
2017-01-25 18:12:14 +0000

    Initial Commit

---
+++ SFMLOpenGL/Game.h
@@ -0,0 +1,39 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/wglew.h>
+
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+
+#include <Debug.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+using namespace std;
+using namespace sf;
+using namespace glm;
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void run();
+private:
+	Window window;
+	bool isRunning = false;
+	void initialize();
+	void update();
+	void render();
+	void unload();
+};
+
+#endif
